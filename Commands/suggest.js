9e710b455ab31b4dd43003eec26e688e529bb428
YT GameWorks <39207327+YT-GameWorks@users.noreply.github.com>
2020-09-19 12:09:02 +0530

    Fully Fixed Suggest.js

--- Commands/suggest.js
+++ Commands/suggest.js
@@ -1,40 +1,41 @@
-const Discord = require("discord.js");
-const { execute } = require("./suggest");
+const Discord = require('discord.js');
 
 module.exports = {
-  name: "suggest",
-  description: "Suggest a thing!",
-  coolDown: 10,
-    async execute(client, message, args)  {
-    
-    if(!args.length) {
-      return message.channel.send("Please Give the Suggestion")
+    name: "suggest",
+    description: "Suggest a thing!",
+    coolDown: 10,
+    async execute(client, message, args)
+    {
+
+    if(!args.length) 
+    {
+        return message.channel.send("Please Give the Suggestion");
     }
     
-    let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"))
+    let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
     
     
-    if(!channel) {
-      return message.channel.send("there is no channel with name - suggestions")
+    if(!channel) 
+    {
+      return message.channel.send("there is no channel with name - suggestions");
     }
                                                     
     
-    let embed = new MessageEmbed()
-    .setAuthor("SUGGESTION: " + message.author.tag, message.author.avatarURL())
-    .setThumbnail(message.author.avatarURL())
-    .setColor("#ff2050")
-    .setDescription(args.join(" "))
-    .setTimestamp()
+    const embed = new Discord.MessageEmbed();
+    embed.setAuthor("SUGGESTION: " + message.author.tag, message.author.avatarURL());
+    embed.setThumbnail(message.author.avatarURL());
+    embed.setColor("#ff2050");
+    embed.setDescription(args.join(" "));
+    embed.setTimestamp();
     
     
     channel.send(embed).then(m => {
-      m.react("✅")
-      m.react("❌")
+      m.react("✅");
+      m.react("❌");
     })
     
-
     
-    message.channel.send("Sent Your Suggestion to " + channel)
+    message.channel.send("Sent Your Suggestion to " + channel);
     
   }
-}
+ }   
