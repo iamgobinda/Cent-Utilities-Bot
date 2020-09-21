ffc0cee6f8934bac6a54b665a742d2b6a0c32736
Aktindo <71206933+Aktindo@users.noreply.github.com>
2020-09-21 21:13:53 +0530

    update
    added embed functions and emojis!

--- Commands/Suggestion.js
+++ Commands/Suggestion.js
@@ -4,43 +4,46 @@ module.exports = {
     name: "suggest",
     description: "Suggest a thing!",
     coolDown: 10,
-    async execute(client, message, args)
-    {
-    if(!args.length) 
-    {
-        const Error = new Discord.MessageEmbed();
-        Error.setTitle('Syntax Error !')
+    async execute(client, message, args) {
+        if (!args.length) {
+            const Error = new Discord.MessageEmbed();
+            Error.setTitle('<:redtick:750751681175748608> Syntax Error!');
+            Error.setDescription('Usage - `!suggest <suggestion>');
 
-        return message.channel.send("Please Give the Suggestion");
+            return message.channel.send(Error);
+        }
+
+        let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
+
+
+        if (!channel) {
+            const Error = new Discord.MessageEmbed();
+            Error.setTitle('<:redtick:750751681175748608> Error!');
+            Error.setDescription('There is no channel with name - suggestions');
+            Error.setColor(0xf53d3d);
+
+            return message.channel.send(Error);
+        }
+
+        const embed = new Discord.MessageEmbed();
+        embed.setAuthor("SUGGESTION FROM " + message.author.tag, message.author.avatarURL());
+        embed.setThumbnail(message.author.avatarURL());
+        embed.setColor(0x2abbf5);
+        embed.setDescription(args.join(" "));
+        embed.setTimestamp();
+
+        channel.send(embed).then(m => {
+            m.react(":greentick:750751680613843105");
+            m.react(":redtick:750751681175748608");
+        })
+
+        const sucess = new Discord.MessageEmbed();
+        sucess.setThumbnail(message.author.avatarURL());
+        sucess.setTitle('<:greentick:750751680613843105> Success!');
+        sucess.setDescription('Successfully sent your sugesstion to be decided upon! We will contact you if the suggestion gets approved!');
+        sucess.setColor(0x32ba4b);
+
+        return message.channel.send(sucess);
+
     }
-    
-    let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
-    
-    
-    if(!channel) 
-    {
-        const Error = new Discord.MessageEmbed();
-        Error.setTitle('Error !');
-        Error.setDescription('there is no channel with name - suggestions');
-        Error.setColor(0xf53d3d);
-
-        return message.channel.send(Error);
-    }
-
-    const embed = new Discord.MessageEmbed();
-    embed.setAuthor("SUGGESTION: " + message.author.tag, message.author.avatarURL());
-    embed.setThumbnail(message.author.avatarURL());
-    embed.setColor(0x2abbf5);
-    embed.setDescription(args.join(" "));
-    embed.setTimestamp();
-    
-    channel.send(embed).then(m => {
-      m.react("✅");
-      m.react("❌");
-    })
-    
-    
-    message.channel.send("Sent Your Suggestion to " + `${channel}`);
-    
-  }
- };
+};
