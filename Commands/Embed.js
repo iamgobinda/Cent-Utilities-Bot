f0051db4b82c7ba73c8ad6804ee40e814d25bfa1
Aktindo <71206933+Aktindo@users.noreply.github.com>
2020-09-21 13:15:05 +0530

    Update Embed.js

--- Commands/Embed.js
+++ Commands/Embed.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'embed',
     coolDown: 30,
-	description: 'A command that lets you make your own embeds !!',
+	description: 'A command that lets you make your own embeds!',
     aliases: ['e', 'em'],
     async execute(client ,message, args)
     {
@@ -13,7 +13,7 @@ module.exports = {
         if(!message.member.roles.cache.has('753283575276896337'))
 		{
 			const Error = new Discord.MessageEmbed();
-			Error.setTitle('ERROR!');
+			Error.setTitle('<:redtick:750751681175748608> ERROR!');
 			Error.setDescription('Only Staff can send embeds!');
 			Error.setColor(0xf53d3d);
 
@@ -25,16 +25,19 @@ module.exports = {
             try
             {
                 const embed = new Discord.MessageEmbed();
+                embed.setAuthor(message.author.username);
+                embed.setThumbnail(message.author.displayAvatarURL());
                 embed.setTitle('**ANNOUCEMENT**');
                 embed.setDescription(announcement);
                 embed.setColor('RANDOM');
+                embed.setFooter(timestamp);
         
                 channel.send(embed);
             }
             catch
             {
                 const Error = new Discord.MessageEmbed();
-                Error.setTitle('Syntax Error!');
+                Error.setTitle('<:redtick:750751681175748608> Syntax Error!');
                 Error.setDescription('Usage: `<channel> <annoucement>`');
                 Error.setColor(0xf53d3d);
     
