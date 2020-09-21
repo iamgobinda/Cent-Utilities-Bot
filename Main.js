cfb15beb47e485d77b4390029877ee59a45fa5c0
Aktindo <71206933+Aktindo@users.noreply.github.com>
2020-09-21 18:36:57 +0530

    Update Main.js
    added auto status changer

--- Main.js
+++ Main.js
@@ -38,6 +38,20 @@ client.once('ready', () => {
 	console.log('Bot Online, Running patch: ' + PatchNumber + ' and build: ' + BuildNumber);
 });
 
+const activities_list = [
+    "with the !help command.", 
+    "with the developers console",
+    "with some code", 
+    "with JavaScript"
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
+
+bot.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
+});
+
 // Triggred when user sends a message or we can check for a specific
 // message here.
 client.on('message', message => {
