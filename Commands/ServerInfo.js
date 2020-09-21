80a5beacc44f0dff402cb5ab387bd0032058a6fe
Aktindo <71206933+Aktindo@users.noreply.github.com>
2020-09-21 12:13:44 +0530

    Updated V2.0 sub update
    some more features

--- Commands/ServerInfo.js
+++ Commands/ServerInfo.js
@@ -5,15 +5,19 @@ module.exports = {
     async execute(client ,message, args) 
     {
 		const Discord = require('discord.js');
+		
+		
+		const ServerInfo = new Discord.MessageEmbed();
 
-        const ServerInfo = new Discord.MessageEmbed();
 		ServerInfo.setTitle('**Information about this server.**')
-		ServerInfo.addField('`Server Owner:` ', `**${message.guild.owner}**`, true);
-		ServerInfo.addField('`Server name:` ', `**${message.guild.name}**`, true);
-		ServerInfo.addField('`Total Members:` ', `**${message.guild.memberCount}**`, true);
-		ServerInfo.addField('`Total Channels:` ', `**${message.guild.channels.cache.size}**`, true);
-		ServerInfo.addField('`Creation date:` ', `**${message.guild.createdAt}**`, false);
-		ServerInfo.addField('`Creation Region:` ', `**${message.guild.region}**`);
+		ServerInfo.addField('**👑 Server Owner:** ', `${message.guild.owner}(${message.guild.ownerID} ) `, true);
+		ServerInfo.addField('**Server name:** ', `${message.guild.name}`, true);
+		ServerInfo.addField('**Total Members:** ', `${message.guild.memberCount}`, true);
+		ServerInfo.addField('**Total Channels:** ', `${message.guild.channels.cache.size}`, true);
+		ServerInfo.addField('**Creation date:** ', `${message.guild.createdAt}`, false);
+		ServerInfo.addField('**Creation Region:** ', `${message.guild.region}`);
+		ServerInfo.addField('**Verification Level:** ', `${message.guild.verificationLevel}`, true);
+		ServerInfo.addField('**Total Boosts:** ', `${message.guild.premiumSubscriptionCount}`, true);
 		ServerInfo.setThumbnail('https://media.discordapp.net/attachments/727420256917782602/754271858760613888/unknown.png');
 		ServerInfo.setColor(0x2abbf5);
 
