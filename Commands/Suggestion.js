071d866c70ee6a82e8305dc21fa45cf2115914cd
SOHAMbuban <sinha.soham@outlook.com>
2020-09-21 14:43:30 +0530

    Update Suggestion.js

--- Commands/Suggestion.js
+++ Commands/Suggestion.js
@@ -8,26 +8,15 @@ module.exports = {
     async execute(client, message, args)
     {
         let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
-        let suggestion = args.slice(1).join(" ");
-        try
-        {
-            const embed = new Discord.MessageEmbed();
-            embed.setTitle('New Suggestion !');
-            embed.setDescription(suggestion);
+        let suggestion = args.slice(0).join(" ");
 
-            channel.send(embed).then(m => {
-                m.react("✅");
-                m.react("❌");
-            })
-        }
-        catch
-        {
-            const Error = new Discord.MessageEmbed();
-            Error.setTitle('<:redtick:750751681175748608> Syntax Error!');
-            Error.setDescription('Usage: `suggest <suggestion>`');
-            Error.setColor(0xf53d3d);
-    
-            message.channel.send(Error);
-        }
+        const embed = new Discord.MessageEmbed();
+        embed.setTitle('New Suggestion !');
+        embed.setDescription(suggestion);
+
+        channel.send(embed).then(m => {
+            m.react("✅");
+            m.react("❌");
+        })
     }
 }
