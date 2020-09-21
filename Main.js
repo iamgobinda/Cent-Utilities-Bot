2e5b27d506e964b06a9cde900cffe5b0cf022883
Aktindo <71206933+Aktindo@users.noreply.github.com>
2020-09-21 18:40:17 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -26,17 +26,8 @@ for (const file of commandFiles)
 const cooldowns = new Discord.Collection();
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
-client.once('ready', () => {
-	client.user.setPresence({
-		status: 'online',
-		activity: {
-			name: '!help',
-			type: 'STREAMING',
-			url: 'https://www.twitch.tv/monstercat'
-		}
-	})
-	console.log('Bot Online, Running patch: ' + PatchNumber + ' and build: ' + BuildNumber);
-});
+
+
 
 const activities_list = [
     "with the !help command.", 
@@ -45,11 +36,12 @@ const activities_list = [
     "with JavaScript"
     ]; // creates an arraylist containing phrases you want your bot to switch through.
 
-bot.on('ready', () => {
+client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
         bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    }, 10000); // Runs this every 10 seconds.
+    }, 10000); 
+console.log('Bot Online, Running patch: ' + PatchNumber + ' and build: ' + BuildNumber); // Runs this every 10 seconds.
 });
 
 // Triggred when user sends a message or we can check for a specific
