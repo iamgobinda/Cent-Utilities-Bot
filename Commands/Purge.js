31d339dd241912a8f774676c6e0cd560c1779baf
Aktindo <71206933+Aktindo@users.noreply.github.com>
2020-09-21 12:27:02 +0530

    Update Purge.js

--- Commands/Purge.js
+++ Commands/Purge.js
@@ -12,8 +12,8 @@ module.exports = {
 		if(!message.member.roles.cache.has('753283575276896337'))
 		{
 			const Error = new Discord.MessageEmbed();
-			Error.setTitle('ERROR !');
-			Error.setDescription('Only Staff can purge !');
+			Error.setTitle('<:redtick:750751681175748608> ERROR!');
+			Error.setDescription('Only Staff can purge!');
 			Error.setColor(0xf53d3d);
 
 			message.channel.send(Error);
@@ -23,8 +23,8 @@ module.exports = {
 		if (isNaN(amount)) 
 		{
 			const NotAValidNumber = new Discord.MessageEmbed();
-			NotAValidNumber.setTitle('**Not a valid number !**');
-			NotAValidNumber.setDescription('that doesn\'t seem to be a valid number.');
+			NotAValidNumber.setTitle('**<:redtick:750751681175748608> Not a valid number!**');
+			NotAValidNumber.setDescription('That doesn\'t seem to be a valid number.');
 			NotAValidNumber.setColor(0xf53d3d);
 
 			return message.reply(NotAValidNumber);
@@ -33,7 +33,7 @@ module.exports = {
 		else if (amount <= 1 || amount > 100)
 		{
 			const TooManySelected = new Discord.MessageEmbed();
-			TooManySelected.setTitle('**Too many messages selected !**');
+			TooManySelected.setTitle('**<:redtick:750751681175748608> Too many messages selected!**');
 			TooManySelected.setDescription('You can only input a number between 1 and 99.');
 			TooManySelected.setColor(0xf53d3d);
 
@@ -42,8 +42,8 @@ module.exports = {
 
 		message.channel.bulkDelete(amount, true).catch(err => {
 			const UnkownError = new Discord.MessageEmbed();
-			UnkownError.setTitle('**Unkown Error !**');
-			UnkownError.setDescription('an unkown error ocuured !');
+			UnkownError.setTitle('**<:redtick:750751681175748608> Unkown Error!**');
+			UnkownError.setDescription('An unknown error ocuured!');
 			UnkownError.setColor(0xf53d3d);
 
 			console.error(err);
@@ -52,8 +52,8 @@ module.exports = {
 		var AmountOfMessagesDeleted = amount - 1;
 
 		const Success = new Discord.MessageEmbed();
-		Success.setTitle('**SUCCESS !**');
-		Success.setDescription('Deleted ' + AmountOfMessagesDeleted + " messages !");
+		Success.setTitle('**SUCCESS!**');
+		Success.setDescription('Deleted ' + AmountOfMessagesDeleted + " messages!");
 		Success.setColor(0x32ba4b);
 
 		message.channel.send(Success);
