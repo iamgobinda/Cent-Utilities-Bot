e37343c7630d6707f3b7bba38a8574fbfb8d8799
Aktindo <71206933+Aktindo@users.noreply.github.com>
2020-09-22 11:40:32 +0530

    L O L
    Update v2.1(MAJOR BUG FIX UPDATE)

--- Commands/UserInfo.js
+++ Commands/UserInfo.js
@@ -1,20 +1,18 @@
 module.exports = {
     name: 'userinfo',
     coolDown: 5,
-	description: 'A user Info command that shows the info about a user.',
-	aliases: ['ui', 'useri', 'uinfo', 'whois'],
-    async execute(client ,message, args) 
-    {
+    description: 'A user Info command that shows the info about a user.',
+    aliases: ['ui', 'useri', 'uinfo', 'whois'],
+    async execute(client, message, args) {
         const Discord = require('discord.js');
 
         let ment = message.mentions.users.first();
 
-        if(!ment)
-        {
+        if (!ment) {
             const userEmbed = new Discord.MessageEmbed();
-            userEmbed.setTitle('Incorrect Usage !');
+            userEmbed.setTitle('Incorrect Usage!');
             userEmbed.setDescription('**Usage:**\n`UserInfo <mentioned User>`');
-            userEmbed.setFooter('HINT: to see ur own info use UserInfo <your mentioned name>.')
+            userEmbed.setFooter('TIP: To see your own info use UserInfo <your mentioned name>.')
             userEmbed.setColor(0xf53d3d);
 
             message.channel.send(userEmbed);
@@ -23,12 +21,13 @@ module.exports = {
         }
 
         const userEmbed = new Discord.MessageEmbed();
-        userEmbed.addField('**Username: **', ment.tag);
-        userEmbed.addField('**ID: **', ment.id);
-        userEmbed.addField('**Status: **', ment.presence.status);
+        userEmbed.setAuthor(ment.username + '#' + ment.discriminator, ment.displayAvatarURL());
+        userEmbed.setThumbnail(ment.displayAvatarURL());
+        userEmbed.setColor(`RANDOM`);
         userEmbed.addField('**Created: **', ment.createdAt);
-        userEmbed.setThumbnail(ment.avatarURL);
-        userEmbed.setColor(0x2abbf5);
+        userEmbed.addField('**Username: **', ment.tag);
+        userEmbed.addField('**Status: **', ment.presence.status);
+        userEmbed.addField('**ID: **', ment.id);
 
         message.channel.send(userEmbed);
     },
