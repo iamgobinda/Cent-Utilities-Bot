62b82d0cb9020e86e31350a700e881d9508e4251
SOHAMbuban <sinha.soham@outlook.com>
2020-09-25 08:21:55 +0530

    Updated kick command

--- Commands/Moderator.js
+++ Commands/Moderator.js
@@ -6,6 +6,34 @@ module.exports = {
     aliases: ['m', 'mo'],
     async execute(client, message, args) 
     {
+        /*
+        if(args[0] === 'kick')
+        {
+            const err = new Discord.MessageEmbed();
+            err.setTitle('You don\'t have the permission to kick memebers !');
+            err.setDescription('Only staff can kick members !');
+            err.setColor(0xf53d3d);
+    
+            message.channel.send(err);
+        }
+        */
+        if(args[0] === 'kick')
+        {
+            let memeberid = message.content.substring(message.content.indexOf(' ') + 1);
+            let member = message.guild.members.cache.get(memeberid);
 
+            if(member)
+            {
+                try
+                {
+                    await member.kick();
+                    console.log('A member was kicked !');
+                }
+                catch(err)
+                {
+                    console.log(err);
+                }
+            }
+        }
     },
 };
