659a09fead046929f27cf7e6f4e9e96d397346da
SOHAMbuban <sinha.soham@outlook.com>
2020-09-25 08:45:42 +0530

    Update Kick.js

--- Commands/Kick.js
+++ Commands/Kick.js
@@ -5,32 +5,20 @@ module.exports = {
     description: 'A command that kicks a person !',
     async execute(client, message, args) 
     {
-        if(args[0] === 'kick')
-        {
-            const err = new Discord.MessageEmbed();
-            err.setTitle('You don\'t have the permission to kick memebers !');
-            err.setDescription('Only staff can kick members !');
-            err.setColor(0xf53d3d);
-    
-            message.channel.send(err);
-        }
-       else if(args[0] === 'kick')
-        {
-           let memeberid = message.content.substring(message.content.indexOf(' ') + 1);
-           let member = message.guild.members.cache.get(memeberid);
+        let memeberid = message.content.substring(message.content.indexOf(' ') + 1);
+        let member = message.guild.members.cache.get(memeberid);
 
-            if(member)
-            {
-                try
-                {
-                   await member.kick();
-                   console.log('A member was kicked !');
-                }
-                catch(err)
-                {
-                   console.log(err);
-                }
-            }
-        }
+         if(member)
+         {
+             try
+             {
+                await member.kick();
+                console.log('A member was kicked !');
+             }
+             catch(err)
+             {
+                console.log(err);
+             }
+         }
     },
 };
